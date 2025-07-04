3405784ceb64478af7c195e3309ed3b6378d81cc
Gizem ALTINCI <gizemaltnc@outlook.com>
2025-07-04 22:54:40 +0300

    ....

--- BlogStore.DataAccessLayer/Abstract/IArticleDal.cs
+++ BlogStore.DataAccessLayer/Abstract/IArticleDal.cs
@@ -1,4 +1,4 @@
-﻿using BlogStore.EntityLayer.Entities;
+﻿ using BlogStore.EntityLayer.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +9,6 @@ namespace BlogStore.DataAccessLayer.Abstract
 {
     public interface IArticleDal:IGenericDal<Article>
     {
+        List<Article> GetArticlesWithCategories();
     }
 }
